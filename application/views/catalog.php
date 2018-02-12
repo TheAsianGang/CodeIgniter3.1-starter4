0d3ee792098a652e4146e921389f81728c387c8d
Inochi Tanaka <yunominosencha@gmail.com>
2018-02-11 22:00:17 -0800

    Update catalog.php

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -1,28 +1,16 @@
-<head>
-    <link rel="stylesheet" type="text/css" href="/public/assets/css/default.css">
-</head>
+<table>
 <tbody>
-  <div>
-      <table id="items_table">
-        <tr class="item_header">
-          <th></th>
-          <th><p>Name<p></th>
-          <th><p>Damage</p></th>
-          <th><p>Protection</p></th>
-          <th><p>Weight</p></th>
-        </tr>
+  <div id="all-items" class="items">
     {accessories}
-        <tr class="item_row" >
-          <td><img class="draggable drag-drop " src="{imagepath}" width="100dp"/></td>
+        <tr class="item_row">
+          <td><img class="draggable drag-drop " src="{imagepath}"/></td>
           <td><h4>{name}</h4></td>
-          <td><p>{damage}</p></td>
-          <td><p>{protection}</p></td>
-          <td><p>{weight}</p></td>
+          <td><p>Damage: {damage}</p></td>
+          <td><p>Protection: {protection}</p></td>
+          <td><p>Weight: {weight}</p></td>
         </tr>
       </div>
     {/accessories}
-    </table>
   </div>
 </tbody>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</table>
