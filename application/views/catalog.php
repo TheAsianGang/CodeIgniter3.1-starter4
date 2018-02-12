1cc109c7d18b2843f90a2a10bda0eb4ebeec82b3
InochiTanaka <yunominosencha@gmail.com>
2018-02-11 23:49:40 -0800

    edit table on catalog page

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -1,13 +1,21 @@
 <table>
 <tbody>
   <div id="all-items" class="items">
+      <table id="items_table">
+        <tr class="item_header">
+          <th></th>
+          <th><p>Name<p></th>
+          <th><p>Damage</p></th>
+          <th><p>Protection</p></th>
+          <th><p>Weight</p></th>
+        </tr>
     {accessories}
-        <tr class="item_row">
-          <td><img class="draggable drag-drop " src="{imagepath}"/></td>
+        <tr class="item_row" >
+          <td><img class="draggable drag-drop " src="{imagepath}" height="150dp"/></td>
           <td><h4>{name}</h4></td>
-          <td><p>Damage: {damage}</p></td>
-          <td><p>Protection: {protection}</p></td>
-          <td><p>Weight: {weight}</p></td>
+          <td><p>{damage}</p></td>
+          <td><p>{protection}</p></td>
+          <td><p>{weight}</p></td>
         </tr>
       </div>
     {/accessories}
