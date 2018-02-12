740ba51a2416c16d83510a46e40a0e2ae0dc7290
InochiTanaka <yunominosencha@gmail.com>
2018-02-12 00:31:10 -0800

    edit table on catalop page

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -1,9 +1,8 @@
-<table>
 <tbody>
   <div id="all-items" class="items">
       <table id="items_table">
         <tr class="item_header">
-          <th></th>
+          <th><p>Image</p></th>
           <th><p>Name<p></th>
           <th><p>Damage</p></th>
           <th><p>Protection</p></th>
@@ -11,8 +10,8 @@
         </tr>
     {accessories}
         <tr class="item_row" >
-          <td><img class="draggable drag-drop " src="{imagepath}" height="150dp"/></td>
-          <td><h4>{name}</h4></td>
+          <td><img class="draggable drag-drop " src="{imagepath}" height="200dp" /></td>
+          <td><p>{name}<p></td>
           <td><p>{damage}</p></td>
           <td><p>{protection}</p></td>
           <td><p>{weight}</p></td>
@@ -21,4 +20,3 @@
     {/accessories}
   </div>
 </tbody>
-</table>
